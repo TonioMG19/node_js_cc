b3749f6355a5969db8c437f0ac55cfad38ec66d0
TonioMG19 <toniokuhn2@gmail.com>
2020-05-04 13:58:24 +0200

    tset

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ class Tournoi{
     }
 
     start(){
+        if(!this.concurents || this.concurents.length==0){
+            console.log("Eh bien, je crois qu'il n'y pas grand monde !");
+            return;
+        }
         while (this.concurents.length >= 2){
-            for(let i = 0; i < concurents.length; i++){
+            for(let i = 0; i < this.concurents.length; i++){
                 this.isDied(this.concurents[i],i);
                 this.canLevelUp(this.concurents[i])
             }
             for(let i = 0; i < this.concurents.length-1; i++){
-                
+
             }
+            this.concurents[0].pointsDeVie = 0;
         }
-        if(this.concurents.length == 0){
-            console.log("Eh bien, je crois qu'il n'y pas grand monde !");
-            return;
-        }
-        else{
+
+        if(this.concurents.length == 1){
             this.winner = this.concurents[0];
             console.log(`Le gagnant du tournoi est ${this.winner.nom} !`);
             return;
@@ -28,8 +30,6 @@ class Tournoi{
 
     isDied(perso,i){
         if(perso.pointsDeVie <= 0){
-            nobodyDied = false;
-            loser = perso;
             console.log(`${perso} vient de succomber !`);
             delete this.concurents[i];
             return;
@@ -71,7 +71,8 @@ class Personnage{
 }
 
 myCarac = new Personnage(20,30,40,18,1,"Lucas");
+otherCarac = new Personnage(20,50,6,19,2,"Jean");
 
-monTournoi = new Tournoi([myCarac]);
+monTournoi = new Tournoi([myCarac,otherCarac]);
 
 monTournoi.start()
